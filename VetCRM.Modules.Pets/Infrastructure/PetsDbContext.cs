d770ddbccdbc98ff37723920e8c86a3e492be57f
AlekseyFomichenko <fomichaaf.95@gmail.com>
2026-01-28 13:51:04 +0300

    work with Pets.Infrastructure

---
+++ VetCRM.Modules.Pets/Infrastructure/PetsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VetCRM.Modules.Pets.Domain;
+
+namespace VetCRM.Modules.Pets.Infrastructure
+{
+    public sealed class PetsDbContext : DbContext
+    {
+        public PetsDbContext()
+        {
+        }
+
+        public PetsDbContext(DbContextOptions<PetsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pet> Pets => Set<Pet>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PetsDbContext).Assembly);
+        }
+    }
+}
---
+++ VetCRM.Modules.Pets/Infrastructure/Repositories/PetRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetCRM.Modules.Pets.Application.Contracts;
+using VetCRM.Modules.Pets.Domain;
+
+namespace VetCRM.Modules.Pets.Infrastructure.Repositories
+{
+    public sealed class PetRepository(PetsDbContext db) : IPetRepository
+    {
+        private readonly PetsDbContext _db = db;
+        public async Task AddAsync(Pet pet, CancellationToken cancellationToken)
+        {
+            await _db.AddAsync(pet, cancellationToken);
+        }
+    }
+}
