916d6bd519a34ec2a89106258ad805bdea03b0a4
AlekseyFomichenko <fomichaaf.95@gmail.com>
2026-01-28 14:20:11 +0300

    di for pets in module

--- VetCRM.Modules.Pets/Module.cs
+++ VetCRM.Modules.Pets/Module.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VetCRM.Modules.Pets.Application.Commands;
+using VetCRM.Modules.Pets.Application.Contracts;
+using VetCRM.Modules.Pets.Infrastructure;
+using VetCRM.Modules.Pets.Infrastructure.Repositories;
 
 namespace VetCRM.Modules.Pets
 {
-    internal class Module
+    public static class Module
     {
+        public static IServiceCollection AddPetsModule(this IServiceCollection services,
+                                                       IConfiguration configuration)
+        {
+            services.AddDbContext<PetsDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IPetRepository, PetRepository>();
+
+            services.AddScoped<CreatePetCommandHandler>();
+
+            return services;
+        }
     }
 }
